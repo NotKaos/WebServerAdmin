493c475aed2ab09371acdf7328731d6c640d3e58
NotKaos <jacoblau117@gmail.com>
2023-05-07 17:28:21 -0700

    converting html homepage to react i think its done just need to add other html routes/pages

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
+import Home from './pages/Home';
 
+
+import Header from './components/Header';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
- 
-      </header>
-    </div>
+   <div>
+    <Header />
+    <Home/>
+    
+   </div>
   );
 }
 
